dadbcb8bf234261906dacdb084e37fce052654b0
THCKwarter <3734237+THCKwarter@users.noreply.github.com>
2017-10-02 00:08:16 -0400

    Initial Commit QueensB

---
+++ NQueensB_MWJ.py
@@ -0,0 +1,8 @@
+#NQueensB_MWJ.py
+#ask the user for an N value
+#generate a candidate NQ solution – list(range(n))
+#define a function to count number of conflicts()
+#while number of conflicts in NQ > 0
+    #randomize (or improve) NQ – shuffle!
+#print NQ
+#print number of iterations
